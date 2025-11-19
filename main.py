a1636a469e4663fef05631d35739119f4003346a
lapkozhop <lapkozhop@gmail.com>
2025-11-19 21:26:41 +1000

    Освоил начальные навыки создания телеграм-бота и успешно запустил его

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import telebot
 
 bot = telebot.TeleBot('8502245941:AAGyezJe6PSOS1wYqlFulX7s_c2D0cy8Sx0')
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, message.from_user.first_name)
+
+
+bot.infinity_polling()
